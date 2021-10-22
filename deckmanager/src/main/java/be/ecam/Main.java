6d15ac79829c3af471a16921c4716bbad750f9ca
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2021-10-22 15:02:34 +0200

    Add memoization sample

--- deckmanager/src/main/java/be/ecam/Main.java
+++ deckmanager/src/main/java/be/ecam/Main.java
@@ -1,60 +1,11 @@
 package be.ecam;
 
-import be.ecam.builder.StringBuilderScenario;
-import be.ecam.card.Card;
-import be.ecam.card.Deck;
-import be.ecam.card.Suit;
-import be.ecam.closure.ClosureKt;
-import be.ecam.lambda.NaiveClosure;
-import kotlin.Unit;
-import kotlin.jvm.functions.Function0;
+import static be.ecam.memoization.FibonacciKt.fib;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-        final Card card1 = new Card(1, Suit.SPADE);
-        Card card2 = new Card(2, Suit.SPADE);
-
-        System.out.println(card1);
-
-        List<Card> array = new ArrayList<>();
-        array.add(card1);
-        array.add(card2);
-
-        Deck<Card> deck = new Deck<>();
-        deck.add(card1);
-        deck.add(card2);
-
-        deck.sort();
-
-        System.out.println(deck);
-        deck.shuffle();
-        System.out.println(deck);
-        deck.sort();
-        System.out.println(deck);
-        Card drawnCard = null;
-        try {
-            drawnCard = deck.draw();
-            drawnCard.getSuit();
-        } catch (Deck.NoMoreCardException e) {
-            System.out.println(e.getMessage());
-        }
-
-        new StringBuilderScenario().run();
-
-
-        Runnable counter = NaiveClosure.getCounter();
-        counter.run();
-        counter.run();
-        counter.run();
-        counter.run();
-
-        Function0<Unit> counter1 = ClosureKt.getCounter();
-        counter1.invoke();
-
+        System.out.println(fib(50));
     }
 
 }
---
+++ deckmanager/src/main/java/be/ecam/memoization/Fibonacci.kt
@@ -0,0 +1,15 @@
+package be.ecam.memoization
+
+/**
+ * Returns the n_th element of the Fibonacci sequence
+ */
+fun fib(n: Int): Long {
+    if (n <= 1) return 1L
+    return memoizeIntToInt(::fib)(n - 1) + memoizeIntToInt(::fib)(n - 2)
+}
+
+val cache = mutableMapOf<Int, Long>()
+
+fun memoizeIntToInt(f: (n: Int) -> Long): (Int) -> Long {
+    return { n -> cache.getOrPut(n) { f(n) } }
+}
