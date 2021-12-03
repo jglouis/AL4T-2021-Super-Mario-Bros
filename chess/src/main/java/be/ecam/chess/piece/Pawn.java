1b0456856aecfe8e0b0e4a9365a51058f159f39b
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2021-12-03 14:59:06 +0100

    Test Pawn + Fix implementation

--- chess/src/main/java/be/ecam/chess/piece/Pawn.java
+++ chess/src/main/java/be/ecam/chess/piece/Pawn.java
@@ -16,14 +16,20 @@ public class Pawn extends Piece {
             boolean isStartingCoordinates = (fromY == 1 && getColor() == Color.WHITE)
                     || (fromY == 6 && getColor() == Color.BLACK);
             if (Math.abs(deltaY) == 1 || Math.abs(deltaY) == 2 && isStartingCoordinates) {
+                final int[][] steps;
+                if (Math.abs(deltaY) == 2) {
+                    int yIncr = getColor() == Color.WHITE ? 1 : -1;
+                    steps = new int[][]{{fromX, fromY}, {fromX, fromY + yIncr}, {toX, toY}};
+                } else {
+                    steps = new int[][]{{fromX, fromY}, {toX, toY}};
+                }
                 return new MoveIterator() {
                     int currentStep = 0;
-                    final int[][] steps = new int[][]{{fromX, fromY}, {toX, toY}};
 
                     @Override
                     public int[] nextStep() {
-                        if (this.currentStep >= 1) return null;
-                        return this.steps[++this.currentStep];
+                        if (this.currentStep >= steps.length - 1) return null;
+                        return steps[++this.currentStep];
                     }
                 };
             }
---
+++ chess/src/test/java/be/ecam/chess/piece/PawnTest.java
@@ -0,0 +1,58 @@
+package be.ecam.chess.piece;
+
+import be.ecam.chess.Color;
+import be.ecam.chess.rule.MoveIterator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PawnTest {
+
+    @Test
+    void getMOveIteratorWhite() {
+        Piece whitePawn = new Pawn(Color.WHITE);
+        MoveIterator it = whitePawn.getMoveIterator(0, 1, 0, 2);
+        assertArrayEquals(new int[]{0, 2}, it.nextStep());
+        assertNull(it.nextStep());
+    }
+
+    @Test
+    void getMoveIteratorBlack() {
+        Piece blackPawn = new Pawn(Color.BLACK);
+        MoveIterator it = blackPawn.getMoveIterator(0, 2, 0, 1);
+        assertArrayEquals(new int[]{0, 1}, it.nextStep());
+        assertNull(it.nextStep());
+    }
+
+    @Test
+    void getMoveIteratorWhite2() {
+        Piece whitePawn = new Pawn(Color.WHITE);
+        MoveIterator it = whitePawn.getMoveIterator(0, 1, 0, 3);
+        assertArrayEquals(new int[]{0, 2}, it.nextStep());
+        assertArrayEquals(new int[]{0, 3}, it.nextStep());
+        assertNull(it.nextStep());
+    }
+
+    @Test
+    void getMoveIteratorBlack2() {
+        Piece blackPawn = new Pawn(Color.BLACK);
+        MoveIterator it = blackPawn.getMoveIterator(0, 6, 0, 4);
+        assertArrayEquals(new int[]{0, 5}, it.nextStep());
+        assertArrayEquals(new int[]{0, 4}, it.nextStep());
+        assertNull(it.nextStep());
+    }
+
+    @Test
+    void getMoveIteratorInvalid() {
+        Piece pawn = new Pawn(Color.WHITE);
+        MoveIterator it = pawn.getMoveIterator(0, 1, 7, 0);
+        assertNull(it);
+    }
+
+    @Test
+    void getMoveIteratorInvalid2() {
+        Piece pawn = new Pawn(Color.WHITE);
+        MoveIterator it = pawn.getMoveIterator(0, 3, 0, 5);
+        assertNull(it);
+    }
+}
