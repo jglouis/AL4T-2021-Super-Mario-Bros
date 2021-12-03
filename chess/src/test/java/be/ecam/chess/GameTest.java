f720f6fd56351929079a7642ad78250f42bbb995
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2021-12-03 11:14:16 +0100

    Test several move scenarios

--- chess/src/test/java/be/ecam/chess/GameTest.java
+++ chess/src/test/java/be/ecam/chess/GameTest.java
@@ -3,7 +3,7 @@ package be.ecam.chess;
 import be.ecam.chess.rule.TurnIterator;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
@@ -21,5 +21,41 @@ class GameTest {
         Game game = new Game(board, new TurnIterator());
 
         assertDoesNotThrow(() -> game.move(0, 0, 1, 1));
+    }
+
+    @Test
+    void move_Obstructed() throws IBoard.CellIsNotEmptyException, IBoard.OutOfBoundException {
+        MockBoard board = new MockBoard();
+        MockPiece piece = new MockPiece(Color.WHITE);
+        piece.setIntermediatesSteps(new int[]{1, 1});
+        board.addPiece(piece, 0, 0);
+        board.addPiece(new MockPiece(Color.WHITE), 1, 1);
+        Game game = new Game(board, new TurnIterator());
+
+        assertThrows(RuntimeException.class, () -> game.move(0, 0, 2, 2));
+    }
+
+    @Test
+    void move_Attack() throws IBoard.CellIsNotEmptyException, IBoard.OutOfBoundException {
+        MockBoard board = new MockBoard();
+        MockPiece piece = new MockPiece(Color.WHITE);
+        board.addPiece(piece, 0, 0);
+        board.addPiece(new MockPiece(Color.BLACK), 1, 1);
+        Game game = new Game(board, new TurnIterator());
+
+        assertDoesNotThrow(() -> game.move(0, 0, 1, 1));
+        assertEquals(piece, board.getPiece(1, 1));
+    }
+
+    @Test
+    void move_AttackFriendlyPiece() throws IBoard.CellIsNotEmptyException, IBoard.OutOfBoundException {
+        MockBoard board = new MockBoard();
+        board.addPiece(new MockPiece(Color.WHITE), 0, 0);
+        MockPiece secondPiece = new MockPiece(Color.WHITE);
+        board.addPiece(secondPiece, 1, 1);
+        Game game = new Game(board, new TurnIterator());
+
+        assertThrows(RuntimeException.class, () -> game.move(0, 0, 1, 1));
+        assertEquals(secondPiece, board.getPiece(1, 1));
     }
 }
--- chess/src/test/java/be/ecam/chess/MockPiece.java
+++ chess/src/test/java/be/ecam/chess/MockPiece.java
@@ -3,26 +3,53 @@ package be.ecam.chess;
 import be.ecam.chess.piece.Piece;
 import be.ecam.chess.rule.MoveIterator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * Mock piece for testing.
  * Accepts any move.
  */
 public class MockPiece extends Piece {
+    private int[][] intermediatesSteps;
+
     public MockPiece(Color color) {
         super(color);
     }
 
+    /**
+     * Set intermediates mov steps for the {@link MoveIterator} returned by {@link #getMoveIterator(int, int, int, int)}.
+     *
+     * @param intermediatesSteps the intermediates steps between start and destination
+     */
+    public void setIntermediatesSteps(int[]... intermediatesSteps) {
+        Arrays.stream(intermediatesSteps).filter(step -> step.length != 2).findAny().ifPresent(step -> {
+            throw new IllegalArgumentException("Intermediates steps must be of length 2");
+        });
+        this.intermediatesSteps = intermediatesSteps;
+    }
+
     @Override
     public MoveIterator getMoveIterator(int fromX, int fromY, int toX, int toY) {
+        final int[][] steps;
+        if (intermediatesSteps != null) {
+            List<int[]> stepsList = new ArrayList<>(intermediatesSteps.length + 2);
+            stepsList.add(new int[]{fromX, fromY});
+            stepsList.addAll(List.of(intermediatesSteps));
+            stepsList.add(new int[]{toX, toY});
+            steps = stepsList.toArray(new int[stepsList.size()][]);
+        } else {
+            steps = new int[][]{{fromX, fromY}, {toX, toY}};
+        }
         return new MoveIterator() {
             int currentStep = 0;
-            final int[][] steps = new int[][]{{fromX, fromY}, {toX, toY}};
 
             @Override
             public int[] nextStep() {
                 if (this.currentStep >= 1) return null;
-                return this.steps[++this.currentStep];
+                return steps[++this.currentStep];
             }
         };
     }
